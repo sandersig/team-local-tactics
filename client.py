76247f4466bb619d73399fd0830a15860c675bd9
Sander Sigmundstad <sanderls@icloud.com>
2022-02-25 12:05:21 +0100

    Two clients can now connect to the server using threads.

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
 from socket import socket
+from teamNetworkTactics import *
 
 sock = socket()
 sock.connect(("localhost", 8888))
 
-#sock.sendall(b"Hello, world")
-#data = sock.recv(1024)
+data = sock.recv(1024)
+print(data.decode())
 
-#print(f"Received {data}")
+while True:
+    data = sock.recv(1024) #Receive signal when both players are connected
+    if(data.decode() == "1"):
+        champions = load_some_champs()
+        print_available_champs(champions)
+        while True:
+            prompt = sock.recv(1024)
+            input(prompt.decode())
+        #print overview of champions to player
+        #take input one at a time, and put straight into a list
+        #When done, return this list to the main-program and let it do its result handling
+        
+
+#input_champion(input from one of the clients)
 
--- server.py
+++ server.py
@@ -1,27 +1,51 @@
 from socket import socket, AF_INET, SOCK_STREAM
+from _thread import *
 from teamNetworkTactics import *
+
+players = []
+player1 = []
+player2 = []
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.bind(("localhost", 8888))
 
 sock.listen(2)
 
-#while True:
-conn, addr = sock.accept()
-print(f"Connected by {addr}")
-main()
+def new_client(conn):
+    conn.send(str.encode("Connected")) #Let the player know when they are connected
+    if len(players) == 2:
+        players[0].send(str.encode("1"))
+        players[1].send(str.encode("1"))
+    for _ in range(2):
+        players[0].send(str.encode("Player 1"))
+        data = conn.recv(1024)
+        player1.append(data)
+        print(player1)
+        
+        #input_champion('Player 1', 'red', champions, player1, player2)
+        players[1].send(str.encode("Player 2"))
+        data = conn.recv(1024)
+        player2.append(data)
+        print(player1)
+        #input_champion('Player 2', 'blue', champions, player2, player1)
+        
+        
+#players = {}
+
+while True:
+    conn, addr = sock.accept()
+    print(f"Connected to {addr}")
+    players.append(conn)
+    1#players["Player1"] = conn 
+    start_new_thread(new_client, (conn,)) 
+    #Everytime a client connects, a new thread gets started
+    #Each thread should run its own version of the game
 
 
-#start game
+#start game -> game logic should be client-side
 #need to show the available champions to both players
 #need to prompt "player 1" for their choice of champion
 #when all player have chosen their champions the "playing" of the game can continue
-
-#while True:
-#    data = conn.recv(1024)
-#    if not data:
-#        break
-#    conn.sendall(data)
 
 sock.close()
 
