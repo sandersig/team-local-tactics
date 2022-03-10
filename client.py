465cee9560f9173fe9e0eef148e6747947c858e1
Sander Sigmundstad <sanderls@icloud.com>
2022-03-10 10:54:37 +0100

    Small bugfixes to client.py

--- client.py
+++ client.py
@@ -5,15 +5,19 @@ sock = socket()
 sock.connect(("localhost", 8888))
 
 data = sock.recv(1024)
+print(data.decode())
+
+data = sock.recv(1024)
 
 print('\n'
     'Welcome to [bold yellow]Team Network Tactics[/bold yellow]!'
     '\n'
     'Each player choose a champion each time.'
     '\n')
-data = sock.recv(1024)
+
 print(data.decode()) #Table getting printed
 print('\n')
+
 while True:
     prompt = sock.recv(2048)
     data = input(prompt.decode())
